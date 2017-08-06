f50e3d0b1a20b9ebf7499c8359125ddbd64da9ca
Halil Karakose <halil.karakose@kodfarki.com>
2017-08-06 19:27:12 +0300

    drawer configuration template

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import {DrawerNavigator} from "react-navigation";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const DrawerConfiguration = DrawerNavigator({
+       // Screens will be defined here
+    },
+    {
+        initialRouteName: "First",
+        drawerPosition: "left",
+        contentOptions: {
+            activeTintColor: "blue",
+            inactiveTintColor: "cornflowerblue",
+        }
+    }
+)
+
+export default DrawerConfiguration;
